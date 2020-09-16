6c1efab3b9f99512803baed359a7f622f3be2cc5
JoeZhou <494390816@qq.com>
2020-09-16 22:27:33 +0800

    翻转二叉树
    Signed-off-by: JoeZhou <494390816@qq.com>

---
+++ src/tree/binaryTree/InvertBinaryTree.java
@@ -0,0 +1,27 @@
+package tree.binaryTree;
+
+/**
+ * @author ：ZYJ
+ * @version :1.0.0
+ * 翻转一棵二叉树。
+ * Invert a binary tree.
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @since :2020/9/16 22:16
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        recursion(root);
+        return root;
+    }
+
+    public void recursion(TreeNode root) {
+        if (root == null) return;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        recursion(root.left);
+        recursion(root.right);
+    }
+}
